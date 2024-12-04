c89f8c086e35e18d9889bad16126497ac5f188af
LOGESH R <logeshvicky103@gmail.com>
2024-12-04 18:28:22 +0530

    Create prob_5.py

---
+++ Math and comp oper/prob_5.py
@@ -0,0 +1,25 @@
+'''---> Product of Six
+
+Description
+
+Given six numbers n1, n2,3, n4,5,6. Find their product.
+
+Input Description
+The first and the only line of the input contains 6 numbers, n1,2,3,4, n5, n6.
+
+Constraints
+<= N<= 20
+
+Output Description
+Print a single integer, denoting the product of the six numbers.
+
+Sample Input 1 
+123456
+Sample Output 1 Copy
+720'''
+
+solution:
+
+def calculate_product(one, two, three, four, five, six):
+    print(one * two * three * four * five * six)
+    
