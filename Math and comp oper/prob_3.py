e9b5ddc6d2a138347e3a891fd351548ffacb47d8
LOGESH R <logeshvicky103@gmail.com>
2024-12-04 18:20:07 +0530

    Create prob_3.py

---
+++ Math and comp oper/prob_3.py
@@ -0,0 +1,27 @@
+'''---> Square and sum
+Description
+You are given three numbers, stored in variables with the following names
+one, two, three
+
+You have to find the square of the three numbers, and find the sum of their square values
+
+Note: Square of a number, is a number multiplied by itself. For example, the square of 3, 53 * 3 - 9
+
+Input Description
+The first and the only line of the input contains, the values stored in one, two, three
+
+Output Description
+Print the sum of the squares of the values stored in one, two, three
+
+Sample Input 1 - Copy
+123
+Sample Output 1 Copy
+114'''
+
+solution:
+
+def calculate_sum_of_squares(one, two, three):
+  print((one**2+two**2+three**2))
+    
+   
+    
