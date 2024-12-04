9257ed9dac91d5e39d742ac91382720a4f54469c
LOGESH R <logeshvicky103@gmail.com>
2024-12-04 18:10:54 +0530

    Update prob_1.py

--- Math and comp oper/prob_1.py
+++ Math and comp oper/prob_1.py
@@ -1,1 +1,20 @@
+'''--->Print Masai School
+Description
+You need to print "Masai School" on a line, and print "A Transformation in Education" on a new line.
 
+Input Description
+No input
+
+Output Description
+Print "Masai School" on a line, and Print. 'A Transformation in Education" on a new line.
+
+Sample Input 1 Copy
+No Input
+
+Sample Output 1 Copy
+Masai School
+A Transformation in Education '''
+
+solution:
+print("Masai School")
+print("A Transformation in Education")
