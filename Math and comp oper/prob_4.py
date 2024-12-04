9edc2b0a9918bfbddd00cb4bd97934e2e7765fcd
LOGESH R <logeshvicky103@gmail.com>
2024-12-04 18:23:54 +0530

    Create prob_4.py

---
+++ Math and comp oper/prob_4.py
@@ -0,0 +1,24 @@
+'''---> Multiply by 50
+
+Description
+
+You are given a number stored in a variable, with the name number Multiply the number stored
+in the variable number by 50, and print the result obtained
+
+Input Description
+The first and the only line of the input contains the number stored in the variable
+number
+
+Output Description
+Print a single number, the result obtained by multiplying it by 50
+
+Sample Input 1
+2
+Sample Output 1
+100  '''
+
+solution:
+
+def multiply_by_50(number):
+    print(number*50)
+
