60256ac763bff9ff7706817ced988cf98033dc1c
LOGESH R <logeshvicky103@gmail.com>
2024-12-04 18:16:51 +0530

    Create prob_2.py

---
+++ Math and comp oper/prob_2.py
@@ -0,0 +1,35 @@
+''''---> Double all
+
+Description
+You are given five numbers, stored in the variables with the following names
+one, two, three, four, five
+
+All the values to be doubled, that is, they will get multiplied by 2
+Find the sum of the values stored in
+one, two, three, four, five, after the above operations are performed
+
+Input Description
+The first and the only line of the input contains the values stored in variabies one two, three, four five
+
+Output Description
+Print the sum of the values stored in
+one, two, three four five,, after the required operation is performed
+
+Sample Input 1
+12345
+Sample Output 1
+30'''
+
+solution:
+
+def sum_doubled_values(one, two, three, four, five):
+  print((one*2+two*2+three*2+four*2+five*2))
+  
+
+  
+  
+  
+  
+   
+  
+  
