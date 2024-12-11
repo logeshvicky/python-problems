f939a668f11d8269dbf97b28362e7aeb0fe4cd3e
LOGESH R <logeshvicky103@gmail.com>
2024-12-11 11:38:03 +0530

    Create prob_2.py

---
+++ cond statement and loops L1/prob_2.py
@@ -0,0 +1,30 @@
+Loop 1 to N:
+
+Description
+You are given a number stored in a variable with the name You have to print all the numbers from 1 to num , including num as well.
+
+Print each number on a new 
+
+Input Description
+
+The first and the only line of the input contains the value present in
+
+Output Description
+
+Print all the numbers from 1 to the value stored inNum, including numas well, on a new line.
+  
+Sample input 
+5
+Sample output 
+1
+2
+3
+4
+5
+
+solution:
+
+def Solve(num):
+    for i in range (1,num+1):
+        print(i)
+  
